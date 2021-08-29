d5d4b8e2dc33cabc4046ac22ec260db55850df17
Nguyễn N. Thiện <kakangocthien109@gmail.com>
2021-08-29 22:07:13 +0700

    software-tools V2.0

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# nguyennt.dev
+if [[ $UID != 0 ]]; then
+    echo "Xin vui long chay lai script voi sudo!"
+    exit 1
+fi
+clear
+echo "Dang cai dat cac phan mem phu thuoc..."
+sudo pacman -Sy --noconfirm --needed figlet lolcat lsb-release git base-devel
+echo "Dang cai dat software-tools..."
+sudo cp software-tools /usr/local/bin/
+echo "De su dung Software Tool, hay dung lenh software-tools."
