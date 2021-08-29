618331ffbabd4bc5d6c2cadca76914ce6f9b47c9
Nguyễn N. Thiện <kakangocthien109@gmail.com>
2021-08-29 22:51:03 +0700

    Fonts update

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # nguyennt.dev
-if [[ $UID != 0 ]]; then
-    echo "Xin vui long chay lai script voi sudo!"
-    exit 1
-fi
 clear
+cp cybersmall.flf ~/.fonts/
+cp cyberlarge.flf ~/.fonts/
+figlet -t  "Software Tool" | lolcat
+figlet -t  "Version 2.0" | lolcat
 echo "Dang cai dat cac phan mem phu thuoc..."
 sudo pacman -Sy --noconfirm --needed figlet lolcat lsb-release git base-devel
 echo "Dang cai dat software-tools..."
