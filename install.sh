0007a727eaa6a52649ebc8a306f70bfc6a5e8a29
Nguyễn N. Thiện <kakangocthien109@gmail.com>
2021-08-30 20:38:45 +0700

    Software-tool update with kernels, D.Env and D.Manager

--- install.sh
+++ install.sh
@@ -2,12 +2,15 @@
 
 # nguyennt.dev
 clear
+mkdir ~/.fonts
+clear
 cp cybersmall.flf ~/.fonts/
 cp cyberlarge.flf ~/.fonts/
+clear
 figlet -t  "Software Tool" | lolcat
 figlet -t  "Version 2.0" | lolcat
 echo "Dang cai dat cac phan mem phu thuoc..."
 sudo pacman -Sy --noconfirm --needed figlet lolcat lsb-release git base-devel
 echo "Dang cai dat software-tools..."
-sudo cp software-tools /usr/local/bin/
-echo "De su dung Software Tool, hay dung lenh software-tools."
+sudo cp software-tool /usr/local/bin/
+echo "De su dung Software Tool, hay dung lenh software-tool."
