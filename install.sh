70266b05ab119822ff671cfd22a3e7cddf4a21ff
Nguyễn N. Thiện <kakangocthien109@gmail.com>
2022-01-23 19:54:00 +0700

    .

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-# nguyennt.dev
-clear
-mkdir ~/.fonts
-clear
-cp cybersmall.flf ~/.fonts/
-cp cyberlarge.flf ~/.fonts/
-clear
 figlet -t  "Software Tool" | lolcat
 figlet -t  "Version 2.0" | lolcat
+
+echo "Dang cai dat font..."
+mkdir ~/.fonts
+cp cybersmall.flf ~/.fonts/
+cp cyberlarge.flf ~/.fonts/
 echo "Dang cai dat cac phan mem phu thuoc..."
 sudo pacman -Sy --noconfirm --needed figlet lolcat lsb-release git base-devel
 echo "Dang cai dat software-tools..."
 sudo cp software-tool /usr/local/bin/
+echo "Cai dat hoan thanh."
 echo "De su dung Software Tool, hay dung lenh software-tool."
